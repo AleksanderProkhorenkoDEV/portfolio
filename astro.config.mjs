ba2db683d13a7258809c48a9665c86665be56fe7
aleksander <aprokhorenkodev@gmail.com>
2026-07-29 16:51:30 +0200

    chore: add the new fonts in to the config file of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "JetBrainMono",
+      cssVariable: "--font-jetbrain",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/JetBrainsMono-Bold.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/JetBrainsMono-Light.ttf"],
+            weight: 200,
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/JetBrainsMono-Medium.ttf"],
+            weight: 500,
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/JetBrainsMono-Regular.ttf"],
+            weight: 400,
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
