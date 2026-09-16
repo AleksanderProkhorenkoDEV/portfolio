93dc8e55c36c58f0d6fa7c327f594be6cc8ff10d
Aleksander <aprokhorenkodev@gmail.com>
2026-09-16 13:19:33 +0200

    feat: add react and 3d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [react(), icon()],
   fonts: [
     {
       provider: fontProviders.local(),
